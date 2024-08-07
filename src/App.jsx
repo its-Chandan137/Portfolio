4e4c35973dd4149151fd26bed9b9c69ec97f8e9a
Chandan Swarnakar <itschandan1374@gmail.com>
2024-08-08 03:01:59 +0530

    try change

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <div>
       <div className="ClassBox">
         <p>is It rendering {newX ? "Crow" : "Mouse"}</p>
-        <h2>Hello there</h2>
+        <h2>Hello there!</h2>
       </div>
     </div>
   )
