279516836245e55c05e7b524973ca3a8adcb83b0
Chandan Swarnakar <itschandan1374@gmail.com>
2024-11-11 11:18:10 +0530

    changes in hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import "../../styles/Hero.scss";
 import { Button } from "react-bootstrap";
 
-import facebook from "../../assets/icons/facebook.svg";
-import twitter from "../../assets/icons/twitter.svg";
-import instagram from "../../assets/icons/instagram.svg";
-import linkedin from "../../assets/icons/linkedin.svg";
-import heroImage from "../../assets/images/hero.png";
+// import facebook from "../../assets/icons/facebook.svg";
+// import twitter from "../../assets/icons/twitter.svg";
+// import instagram from "../../assets/icons/instagram.svg";
+// import linkedin from "../../assets/icons/linkedin.svg";
+
+import heroImage from "../../assets/images/Chandan image fill.png";
+import heroImageHolo from "../../assets/images/Chandan image holo.png";
+import heroImage2 from "../../assets/images/hero.png";
 import { BodyOne, Title } from "../common/CustomeComponents";
 
 
@@ -15,9 +18,9 @@ export const Hero = () => {
     <div className="hero_section">
       <div className="hero-content section">
         <Title level={3}>Hello, It's Me</Title>
-        <Title level={6}>Little Demon</Title>
-        <Title level={3}>
-          And I'm a <span>Frontend Devloper</span>
+        <Title level={1}>Little Demon</Title>
+        <Title level={3} className={"Live_span_container"}>
+          And I'm a <span>Frontend Developer</span>
         </Title>
         <BodyOne>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
@@ -25,13 +28,25 @@ export const Hero = () => {
         </BodyOne>
 
         <div className="social-media">
-          <img src={facebook} className="social_icon" alt="FB"/>
-          <img src={twitter} className="social_icon" alt="FB"/>
-          <img src={instagram} className="social_icon" alt="FB" />
-          <img src={linkedin} className="social_icon" alt="FB" />
+
+          <a href="#" className="social_icon">
+            <i class='bx bxl-facebook'></i>
+          </a>
+          <a href="#" className="social_icon">
+            <i class='bx bxl-instagram-alt' ></i>
+          </a>
+          <a href="#" className="social_icon">
+            <i class='bx bxl-twitter' ></i>
+          </a>
+          <a href="#" className="social_icon">
+            <i class='bx bxl-linkedin' ></i>
+          </a>
+
         </div>
 
-        <Button>Download Resume</Button>
+        <div>
+          <Button>Download Resume</Button>
+        </div>
 
       </div>
       <div className="hero_image_box">
