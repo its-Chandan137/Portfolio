9f1c83563c2a8eb17fb5a17d9f7682156aee0c81
its_Chandan <158255069+its-Chandan137@users.noreply.github.com>
2025-04-14 11:36:01 +0530

    changes in cards css and logos images added

--- src/screen/Projects/ProjectPage.jsx
+++ src/screen/Projects/ProjectPage.jsx
@@ -3,14 +3,23 @@ import ProjectCard from './ProjectCard';
 import './Projects.scss';
 import fileIcon from '../../../public/images/icons/files.svg';
 import projectImage from '../../../public/images/projectImage1.webp';
-import vibhImage from '../../../public/images/Projects/Vibhu-Image.png';
-import vibhuLogo from '../../../public/images/logos/vibhu-logo-final.png';
-import vibhuIcon from '../../../public/images/logos/vibhuLogoPng.png';
 
+import amplifyLogo from '../../../public/images/logos/Amplify-Logo.png';
+import amplifyImage from '../../../public/images/Projects/Amplify-Infra.png';
+
+import vibhuLogo from '../../../public/images/logos/vibhu-logo-final.png';
+import vibhImage from '../../../public/images/Projects/Vibhu-Image.png';
+
+import internBoxLogo from '../../../public/images/logos/InternboxNewLogo.png';
+import internBoxImage from '../../../public/images/Projects/InternBoxImage.png';
+
+
+
+  
 const projects = [
   { id: '01', title: 'Vibhu', logo: vibhuLogo, image: vibhImage },
-  { id: '02', title: 'Gloster', logo: fileIcon, image: projectImage },
-  { id: '03', title: 'Linea Vol.I', logo: fileIcon, image: projectImage },
+  { id: '02', title: 'Amplify', logo: amplifyLogo, image: amplifyImage },
+  { id: '03', title: 'InternBox', logo: internBoxLogo, image: internBoxImage },
   { id: '04', title: 'Cube 2.0', logo: fileIcon, image: projectImage },
 ];
 
@@ -18,10 +27,43 @@ export default function ProjectPage() {
   return (
     <div className="container">
       <div className="project-page">
-        {projects.map(project => (
-          <ProjectCard key={project.id} {...project} />
-        ))}
+        <div className="project-page-card-container">
+          {projects.map(project => (
+            <ProjectCard key={project.id} {...project} />
+          ))}
+          <div className="card-container-background">
+            <div className="card-section-background"></div>
+            <div className="card-section-background"></div>
+            <div className="card-section-background wavy-box">
+              <svg
+                className="wavy-pattern"
+                viewBox="0 0 300 350"
+                preserveAspectRatio="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {Array.from({ length: 80 }).map((_, i) => {
+                  const startX = 10 + i * 10; // Shift starting X to spread across
+                  const cp1X = startX - 190;
+                  const cp2X = startX - 100;
+                  const endX = startX - 200;
+                  const strokeWidth = i % 4 === 0 ? 2.5 : 1.3; // 1 in 3 is thick
+                  return (
+                    <path
+                      key={i}
+                      d={`M${startX} 0 C${cp1X} 100, ${cp2X} 1000, ${endX} 350`}
+                      stroke="#00eeff81"
+                      strokeWidth={strokeWidth}
+                      fill="none"
+                    />
+                  );
+                })}
+              </svg>
+            </div>
+            <div className="card-section-background"></div>
+          </div>
+        </div>
       </div>
+      
     </div>
   );
 }
