f2d4eb387850ead44b52eb34c9b2947d9404c340
its_Chandan <158255069+its-Chandan137@users.noreply.github.com>
2026-04-17 21:29:15 +0530

    Header nav and header links css

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Header.scss';
 
 export const Header = () => {
+  const location = useLocation();
+
+
   const navItems = [
     { to: '/', label: 'Home' },
     { to: '/about', label: 'About' }, // Disabled link
@@ -10,10 +13,18 @@ export const Header = () => {
     { to: '/contact', label: 'Contact'},
   ];
 
+  const navThere = (to) => {
+    if (location.pathname === to) {
+      console.warn("Already on this page");
+      return; 
+    }
+    window.location.href = to;
+  };
+
   return (
     <div className='container'>
       <div className="header">
-        <a href="/" className="logo">Chandan Swarnakar</a>
+        <a onClick={() => navThere('/')} className="logo">Chandan Swarnakar</a>
         <nav className="navbar">
           {navItems.map((item, index) => (
             <NavLink
